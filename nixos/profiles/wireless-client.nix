499f595331abb6edcf076755c2a50821135f9157
Eric Drechsel <eric@pdxhub.org>
2024-02-15 00:41:04 -0800

    fix some warnings

--- nixos/profiles/wireless-client.nix
+++ nixos/profiles/wireless-client.nix
@@ -34,6 +34,6 @@
   services.avahi.enable = true;
   # Important to resolve .local domains of printers, otherwise you get an error
   # like  "Impossible to connect to XXX.local: Name or service not known"
-  services.avahi.nssmdns = true;  # gate settings
+  services.avahi.nssmdns4 = true;  # gate settings
   
 }
